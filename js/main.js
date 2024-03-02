1b42782c2750717adbad3baae0b7ca2e3d36e8f5
AliHoushyar <alihkhoshdell@gmail.com>
2024-03-02 09:17:17 +0330

    fixed responsive

--- js/main.js
+++ js/main.js
@@ -136,6 +136,7 @@ function scrollFunction() {
       if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         fix.style.backgroundColor = "white";
         fix.style.height = "60px";
+        document.getElementById('linkedcomp').style.color = "#7825cb";
         document.getElementsByClassName("signin_link")[0].style.color = "#7825cb";
         document.getElementsByClassName("nav-btn-stay")[0].style.borderColor = "#7825cb";
         document.getElementsByClassName("login_icon")[0].style.color = "#7825cb";
@@ -152,14 +153,15 @@ function scrollFunction() {
         fix.style.backgroundColor = "rgba(0, 0, 0, 0.475)";
         fix.style.height = "90px";
         let links = document.getElementsByClassName("nav-link");
+        document.getElementById('linkedcomp').style.color = "white";
         document.getElementsByClassName("signin_link")[0].style.color = "white";
         document.getElementsByClassName("nav-btn-stay")[0].style.borderColor = "white";
         document.getElementsByClassName("login_icon")[0].style.color = "white";
         for(let i =0;i<5;i++){
             links[i].style.color = "white";
         }
-        document.getElementById("navlogo").style.height = "60px";
-        document.getElementById("navlogo").style.width = "70px";
+        document.getElementById("navlogo").style.height = "55px";
+        document.getElementById("navlogo").style.width = "60px";
         fix.style.boxShadow = "none";
         $("#navlogo").attr("src","../img/logo3.png")
       }
