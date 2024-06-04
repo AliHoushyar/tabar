16284ff3c1db64bf294859ef72bb4507581a3de0
AliHoushyar <alihkhoshdell@gmail.com>
2024-06-04 18:00:35 +0330

    responses fixed

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ function scrollFunction() {
         document.getElementsByClassName('linkedcomp')[2].style.color = "#7825cb";
         document.getElementsByClassName('linkedcomp')[3].style.color = "#7825cb";
         document.getElementsByClassName('linkedcomp')[4].style.color = "#7825cb";
+        document.getElementsByClassName('linkedcomp')[5].style.color = "#7825cb";
         document.getElementsByClassName("signin_link")[0].style.color = "#7825cb";
         document.getElementsByClassName("nav-btn-stay")[0].style.borderColor = "#7825cb";
         document.getElementsByClassName("login_icon")[0].style.color = "#7825cb";
@@ -151,7 +152,7 @@ function scrollFunction() {
         document.getElementById("navlogo").style.height = "40px";
         document.getElementById("navlogo").style.width = "40px";
         fix.style.boxShadow = "1px 9px 37px -2px rgba(0,0,0,0.75)";
-        $("#navlogo").attr("src","../img/logo.png")
+        $("#navlogo").attr("src","../img/logo3.png")
       }
       else{
         fix.style.backgroundColor = "rgba(0, 0, 0, 0.475)";
@@ -162,6 +163,7 @@ function scrollFunction() {
         document.getElementsByClassName('linkedcomp')[2].style.color = "white";
         document.getElementsByClassName('linkedcomp')[3].style.color = "white";
         document.getElementsByClassName('linkedcomp')[4].style.color = "white";
+        document.getElementsByClassName('linkedcomp')[5].style.color = "white";
         document.getElementsByClassName("signin_link")[0].style.color = "white";
         document.getElementsByClassName("nav-btn-stay")[0].style.borderColor = "white";
         document.getElementsByClassName("login_icon")[0].style.color = "white";
@@ -171,7 +173,7 @@ function scrollFunction() {
         document.getElementById("navlogo").style.height = "55px";
         document.getElementById("navlogo").style.width = "60px";
         fix.style.boxShadow = "none";
-        $("#navlogo").attr("src","../img/logo3.png")
+        $("#navlogo").attr("src","../img/logo.png")
       }
 }
 
